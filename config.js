76dbfa8d02d7db91ffda382b2747476a77e1aebd
ajis9 <104950959+ajis9@users.noreply.github.com>
2022-05-05 12:20:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 
 // Other
 global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.premium = ['628812830965']
 global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.author = 'ajis Botz'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
